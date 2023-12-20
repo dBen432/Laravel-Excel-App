37466b24b50a68dcd00f4801ef81ddeb718f16c3
Dennis Benny <dennisbenny6@gmail.com>
2023-12-20 23:54:23 +0530

    Update database connection

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL=parse_url('postgres://eaifbkoonnfkaf:4320204589b130db008f58601c54feca04505d974008e1873d1035ded2c47ca7@ec2-52-4-153-146.compute-1.amazonaws.com:5432/d7h5lvsgollu2p');
 
 return [
 
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -81,11 +82,11 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL['user'],,
+            'password' => $DATABASE_URL['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
