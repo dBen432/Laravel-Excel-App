b147c5245c39cf35f0d8f17e2d23fb6b83612b6f
Dennis Benny <dennisbenny6@gmail.com>
2023-12-20 23:36:53 +0530

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+ 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+ 
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+Route::get('list', [EmployeeController::class, 'index']);
+Route::get('export', [EmployeeController::class, 'export']);
+
+Route::post('store', [EmployeeController::class, 'store']);
+Route::post('edit', [EmployeeController::class, 'edit']);
+Route::post('delete', [EmployeeController::class, 'destroy']);
